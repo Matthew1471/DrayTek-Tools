73d95d2d2ef368a625d95101ad46eb538cdd7890
Matthew <5273302+Matthew1471@users.noreply.github.com>
2024-12-25 00:01:53 +0000

    Add EdgeRouter Example
    - Add EdgeRouter route-test example.

---
+++ Python/examples/edgerouter/draytek_health.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of DrayTek-Tools <https://github.com/Matthew1471/DrayTek-Tools>
+# Copyright (C) 2024 Matthew1471!
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+This listens for a DrayTek® Vigor DSL Status message broadcast, decrypts and parses it.
+If the modem is reporting success then it returns a 0, otherwise it returns non-zero and EdgeOS
+may then choose to initiate WAN failover.
+"""
+
+# We use the system socket APIs to listen for network traffic.
+import socket
+
+# We have to rely on calling OpenSSL as we cannot install packages.
+import subprocess
+
+# The program arguments are read.
+import sys
+
+
+# Get the key for your specific modem from the keygen script.
+DECRYPT_KEY = '31424143373742324339'
+
+# Check whether the user has invoked this directly.
+if len(sys.argv) != 4:
+    print('Usage: {} <load_balance_group> <test_interface> <current_status>').format(sys.argv[0])
+    sys.exit(1)
+
+# Create a UDP socket to listen for DSL Status messages.
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Set a timeout of 11 seconds (the modem should broadcast every 10 seconds).
+sock.settimeout(11)
+
+# Bind to all interfaces on port 4944.
+sock.bind(('0.0.0.0', 4944))
+
+# Maximum number of bytes to receive.
+max_receive_bytes = 116
+
+try:
+    # If a payload of the incorrect length is received we try again.
+    while True:
+        # Attempt to receive a broadcast packet.
+        receive_buffer, ip_address = sock.recvfrom(max_receive_bytes)
+
+        # Check to see if this would be the right length for a DSL Status message.
+        if len(receive_buffer) == 116:
+            # Run the OpenSSL command using subprocess to perform the decryption.
+            process = subprocess.Popen([
+                "openssl", "enc", "-d", "-aes-128-cbc",
+                "-K", DECRYPT_KEY,
+                "-iv", DECRYPT_KEY,
+                "-nopad"
+            ], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            # Pass the encrypted data through stdin and obtain the output and any errors.
+            stdout, stderr = process.communicate(input=receive_buffer[4:])
+
+            # Did OpenSSL decrypt the contents?
+            if process.returncode == 0:
+                # Only obtain the status (this script needs to be performant)
+                decrypted_status = str(stdout[86:-14].split(b'\0')[0])
+
+                # Check the DSL status.
+                if decrypted_status == 'SHOWTIME':
+
+                    # If the connection is not marked as currently okay, log that it now seems okay.
+                    if sys.argv[3] != 'OK':
+                        subprocess.call([
+                            'logger',
+                            '-t draytek_health',
+                            'WLB: Load-Balance group {} interface {} ({}) DSL status now good.'.format(
+                                sys.argv[1],
+                                sys.argv[2],
+                                sys.argv[3]
+                            )
+                        ])
+
+                    # Return Success.
+                    sys.exit(0)
+                else:
+                    # Failed; Write to log.
+                    subprocess.call([
+                        'logger',
+                        '-t draytek_health',
+                        'WLB: Load-Balance group {} interface {} ({}) DSL status bad ({}).'.format(
+                            sys.argv[1],
+                            sys.argv[2],
+                            sys.argv[3],
+                            str(decrypted_status)
+                        )
+                    ])
+
+                    # Return failure.
+                    sys.exit(1)
+            # Failed to decrypt (DSL status unknown).
+            else:
+                # OpenSSL issue; Write to log.
+                subprocess.call([
+                    'logger',
+                    '-t draytek_health',
+                    'WLB: Decryption failed "{}".'.format(stderr)
+                ])
+
+                # Return a DSL Status success out of caution.
+                sys.exit(0)
+
+except socket.timeout:
+
+    # Failed; Write to log.
+    subprocess.call([
+        'logger',
+        '-t draytek_health',
+        'WLB: Load-Balance group {} interface {} ({}) Timeout waiting for DSL status.'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+         )
+    ])
+
+    # Return failure.
+    sys.exit(1)
+finally:
+    # Clean up any resources.
+    sock.close()
---
+++ Python/examples/edgerouter/draytek_keygen.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of DrayTek-Tools <https://github.com/Matthew1471/DrayTek-Tools>
+# Copyright (C) 2024 Matthew1471!
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+This script generates in advance the key needed to decrypt DrayTek DSL Status broadcasts.
+
+The key is not calculated by draytek_health.py to reduce computational workload when it is run
+every >10 seconds.
+"""
+
+# The hex characters of the SHA-1 digest make up the encryption/decryption key.
+import binascii
+
+# Performs the SHA-1 hash of the MAC address.
+import hashlib
+
+# The program arguments are read.
+import sys
+
+# Check whether the user has supplied a source MAC address.
+if len(sys.argv) != 2:
+    print('Usage:')
+    print('{} <MAC Address of Vigor DSL Modem>'.format(sys.argv[0]))
+    print('e.g. {} aa:bb:cc:dd:ee:ff'.format(sys.argv[0]))
+    sys.exit(1)
+
+# Get the user supplied MAC address from the command line arguments.
+mac_address = sys.argv[1]
+
+# If the MAC address is in string form it needs to be converted to bytes.
+if isinstance(mac_address, str):
+    mac_address = binascii.unhexlify(mac_address.replace(':', ''))
+
+# The encryption/decryption key is the first 5 bytes from the SHA-1 digest of the MAC
+# address bytes.
+digest = hashlib.sha1(mac_address).digest()[:5]
+
+# Get the uppercase (Python prefers lowercase) hexadecimal characters of the digest
+# (5 bytes is 10 characters).
+digest = binascii.hexlify(digest).upper()
+
+# Get the uppercase hexadecimal characters for the hexadecimal characters
+# (to be supplied to OpenSSL as -K).
+digest = binascii.hexlify(digest).upper()
+
+# Output the decryption key.
+print('DECRYPT_KEY = {}'.format(digest.decode()))
